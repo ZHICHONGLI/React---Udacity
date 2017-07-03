ece7c5baab5bd3316a071fe1400d6850a82d4618
Zhichong Li <zhichongli@Zhichongs-iMac.local>
2017-07-02 23:43:44 -0700

    using state

--- contactlist/src/App.js
+++ contactlist/src/App.js
@@ -23,10 +23,32 @@ const contacts = [
 ]
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <ListContacts contacts={contacts}/>
+        <ListContacts contacts={this.state.contacts}/>
       </div>
     );
   }
