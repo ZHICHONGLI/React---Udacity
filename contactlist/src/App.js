74b5bb57d907ecd6247d15e5b3ea7ee2acc00bc8
Zhichong Li <zhichongli@Zhichongs-iMac.local>
2017-07-05 22:50:36 -0700

    add search and show all

--- contactlist/src/App.js
+++ contactlist/src/App.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
-
 class App extends Component {
   state = {
     contacts: [
@@ -45,10 +24,18 @@ class App extends Component {
       }
     ]
   }
+  removeContact = (contact) => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(c => c.id !== contact.id)
+    }))
+  }
   render() {
     return (
       <div className="App">
-        <ListContacts contacts={this.state.contacts}/>
+        <ListContacts
+          onDeleteContact={this.removeContact}
+          contacts={this.state.contacts}
+        />
       </div>
     );
   }
